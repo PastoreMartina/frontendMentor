31f2ac9dc92b35e83cfe656885164fe774b7cd6d
PastoreMartina <martina.pastore29@gmail.com>
2023-03-03 00:02:29 +0100

    Set background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ['./build/index.html'],
   theme: {
+    colors: {
+      'orange' : 'hsl(31, 77%, 52%)',
+      'cyan' : 'hsl(184, 100%, 22%)',
+      'dark-cyan' : 'hsl(179, 100%, 13%)',
+    },
     extend: {
     },
   },
