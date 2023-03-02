d97c9721daa0832177e28ad986cb756556501baa
PastoreMartina <martina.pastore29@gmail.com>
2023-03-02 22:26:23 +0100

    Fixed font issue

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ['./build/*.html'],
   theme: {
+    colors: {
+      'orange': 'hsl(31, 77%, 52%)',
+      'cyan' : 'hsl(184, 100%, 22%)',
+      'dark-cyan' : 'hsl(179, 100%, 13%)',
+      'white': 'hsla(0, 0%, 100%, 0.75)',
+      'gray' : 'hsl(0, 0%, 95%)',
+    },
     extend: {
     },
   },
