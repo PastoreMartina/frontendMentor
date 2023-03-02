1e8c00726f3d4c0cf7e5b28fd6bef00e19d861d4
PastoreMartina <martina.pastore29@gmail.com>
2023-03-03 00:13:59 +0100

    Set text color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
       'orange' : 'hsl(31, 77%, 52%)',
       'cyan' : 'hsl(184, 100%, 22%)',
       'dark-cyan' : 'hsl(179, 100%, 13%)',
+      'tr-white' : 'hsla(0, 0%, 100%, 0.75)',
+      'white' : 'hsl (0, 0%, 100%)',
     },
     extend: {
     },
